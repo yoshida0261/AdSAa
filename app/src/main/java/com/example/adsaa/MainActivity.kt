4cb49a5a4de82b6c4ae81377d282897c844d963c
yoshida0261 <yoshida261@gmail.com>
2019-10-28 11:34:04 +0900

    feat: interstitial広告を追加

--- app/src/main/java/com/example/adsaa/MainActivity.kt
+++ app/src/main/java/com/example/adsaa/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.adsaa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var mPublisherInterstitialAd: PublisherInterstitialAd
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +21,17 @@ class MainActivity : AppCompatActivity() {
         val adRequest = PublisherAdRequest.Builder().build()
         publisherAdView.loadAd(adRequest)
         //mPublisherAdView.loadAd(adRequest)
+
+        mPublisherInterstitialAd = PublisherInterstitialAd(this)
+        mPublisherInterstitialAd.adUnitId = "/6499/example/interstitial"
+        mPublisherInterstitialAd.loadAd(PublisherAdRequest.Builder().build())
+
+        textView.setOnClickListener {
+            if (mPublisherInterstitialAd.isLoaded) {
+                mPublisherInterstitialAd.show()
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.")
+            }
+        }
     }
 }
